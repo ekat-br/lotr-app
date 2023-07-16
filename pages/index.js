183cb714e9d23eb0936a852be9498e4b16d6973b
Ekaterina Bruch <ekaterinabruch@gmail.com>
2023-07-16 21:36:47 +0200

    Add introduction text for deployment

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,41 @@
 import Link from "next/link";
 import { GlobalStyles } from "@/styles";
+import styled from "styled-components";
 
 export default function HomePage() {
+  const VolumesHeadline1 = styled.h1`
+    font-family: Lora;
+    font-size: 44px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 48px;
+    color: var(--earth, #282828);
+  `;
+  const VolumesIntroText = styled.p`
+    align-self: stretch;
+    color: var(--earth, #282828);
+    font-family: Lora;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+  `;
+  const AllVolumesLink = styled(Link)`
+    font: var(--font-body);
+    color: var(--color-earth);
+    text-decoration: none;
+  `;
+
   return (
     <>
-      <Link href="/volumes">Go to all volumes</Link>
+      <VolumesHeadline1>Lord of the Rings App</VolumesHeadline1>
+      <VolumesIntroText>
+        Hi, this is a small fanpage of the book series "Lord of the Rings".
+        Currently the website is still in the final polish. But you can already
+        have a look around.
+      </VolumesIntroText>
+
+      <AllVolumesLink href="/volumes">Go to all volumes →</AllVolumesLink>
     </>
   );
 }
